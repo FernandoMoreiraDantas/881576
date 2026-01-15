2e191e5cad3f463049e7ec402abe01f74e09b490
unknown <fernando.moreiradantas@gmail.com>
2026-01-15 11:46:20 -0300

    Implementa~]ao da funcionalidade de alterar um artista selecionado.

--- src/main/java/com/seplag/controller/ArtistaController.java
+++ src/main/java/com/seplag/controller/ArtistaController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +37,21 @@ public class ArtistaController {
     public ResponseEntity<Artista> buscar(@PathVariable Long id) {
         return repository.findById(id)
                 .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Artista> atualizar(@PathVariable Long id, @RequestBody Artista dadosAtualizados) {
+        return repository.findById(id)
+                .map(artista -> {
+                    // Atualiza os campos necessários
+                    artista.setNome(dadosAtualizados.getNome());
+                    artista.setTipo(dadosAtualizados.getTipo());
+                    
+                    // Salva a alteração
+                    Artista artistaSalvo = repository.save(artista);
+                    return ResponseEntity.ok(artistaSalvo);
+                })
                 .orElse(ResponseEntity.notFound().build());
     }
 
